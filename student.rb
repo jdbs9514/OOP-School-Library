f0032072e57f0d919a6cc4bd7e6bf7a8130db17a
jdbs9514 <jdbs9514@gmail.com>
2022-10-11 19:29:49 -0500

    decorate my Person class

--- student.rb
+++ student.rb
@@ -1,7 +1,7 @@
 require_relative './person'
 
 class Student < Person
-  attr_accesor :classroom
+  attr_accessor :classroom
 
   def initialize(age, classroom, name: 'unknow', parent_permission: true)
     super(age: age, name: name, parent_permission: parent_permission)
