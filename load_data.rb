8a719757c48622caaef9fae67ac9b030a61d35e4
jdbs9514 <jdbs9514@gmail.com>
2022-10-19 17:02:32 -0500

    Fix error in load data file

--- load_data.rb
+++ load_data.rb
@@ -2,18 +2,18 @@ class LoadData
   def self.read_books
     file_path = './DATA/books.json'
     return unless File.exist?(file_path)
-​
+
     books_file = File.open(file_path)
     books = []
     JSON.parse(books_file.read).each { |book| books << Book.new( book['id'], book['title'], book['author']) }
     books_file.close
     books
   end
-​
+
   def self.read_people
     file_path = './DATA/people.json'
     return unless File.exist?(file_path)
-​
+
     people_file = File.open(file_path)
     people = []
     JSON.parse(people_file.read).each do |person|
@@ -25,7 +25,7 @@ class LoadData
                       person['id'], 
                       parent_permission: person['parent_permission']
                     ))
-​
+
       elsif person['type'] == 'Teacher'
         people.push(Teacher.new(
                     person['specialization'],
@@ -35,11 +35,11 @@ class LoadData
                     ))
       end
     end
-​
+
     people_file.close
     people
   end
-​
+
   def self.read_rentals(books, people)
     file_path = './DATA/rentals.json'
     return unless File.exist?(file_path)
@@ -50,7 +50,7 @@ class LoadData
     JSON.parse(rentals_file.read).each do |rental|
       book = books.select { |book| book.id == rental['book_id'] }
       rental_person = people.select { |person| person.id == rental['person_id'] }
-​
+
       rentals.push(Rental.new(book[0], rental_person[0], rental['date'], rental['id']))
     end
     rentals_file.close
